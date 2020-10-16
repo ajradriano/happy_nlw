6399e69d4d3daaf2aadd30913b3d9cb879c38224
Adriano Rodrigues <adriano@ingadigital.com.br>
2020-10-16 00:51:53 -0300

    finalizado map + component sidebar + add pages create e edit item

--- www/src/routes.tsx
+++ www/src/routes.tsx
@@ -3,6 +3,10 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 import Landing from './pages/landing';
 import OrphanagesMap from './pages/orphanage-map';
+import Orphanage from './pages/orphanage';
+import CreateOrphanage from './pages/createOrphanage';
+
+
 
 function Routes() {
     return (
@@ -10,6 +14,9 @@ function Routes() {
             <Switch>
                 <Route path="/" exact component={Landing} />
                 <Route path="/app" component={OrphanagesMap} />
+                <Route path="/orphanages/create" component={CreateOrphanage} />
+                <Route path="/orphanages/:id" component={Orphanage} />
+
             </Switch>
         </BrowserRouter>
     )
